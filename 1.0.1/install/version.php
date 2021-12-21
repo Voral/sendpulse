fbca40cac0b6f468273538cd7cfbf6e9dde6f2d4
Alexander Vorobyev <voral.adm@gmail.com>
2021-12-21 13:20:29 +0300

    Мусор из репы

--- 1.0.1/install/version.php
+++
@@ -1,5 +0,0 @@
-<?php
-$arModuleVersion = array(
-	'VERSION' => '1.0.1',
-	'VERSION_DATE' => '2017-12-28'
-);
--- 1.0.2/install/version.php
+++
@@ -1,5 +0,0 @@
-<?php
-$arModuleVersion = array(
-	'VERSION' => '1.0.2',
-	'VERSION_DATE' => '2018-01-18'
-);
